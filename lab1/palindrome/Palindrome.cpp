88e51fe486ad997f6d59d8377a78acb24541849c
Jan <jan-zasadny@wp.pl>
2018-03-05 21:48:04 +0100

    Utworzono program sprawdzający palindrom

--- lab1/palindrome/Palindrome.cpp
+++ lab1/palindrome/Palindrome.cpp
@@ -7,7 +7,16 @@
 
 bool is_palindrome(std::string str){
     bool czypal= true;
+    int dl=str.size()-1;
+    int licznik=0;
+    while(licznik<dl&&czypal==true){
+        if(str[licznik]!=str[dl]){
+            czypal=false;
 
-
+        }
+        dl--;
+        licznik++;
+    }
+    return czypal;
 
 };
--- lab1/palindrome/main.cpp
+++ lab1/palindrome/main.cpp
@@ -1,5 +1,27 @@
 //
 // Created by Jan on 05.03.2018.
 //
-
+#include <iostream>
 #include <string>
+#include "Palindrome.h"
+
+
+int main(){
+
+    std::cout << "Podaj opcje: 1-Test na palindrom 0-Wyjście";
+    int opcja;
+    std::cin >> opcja;
+    if(opcja==1){
+        std::cout << "Podaj tekst";
+        std::string tekst;
+        std::cin >> tekst;
+        if(is_palindrome(tekst)==1){
+            std::cout << "Tak, to palindrom";
+        }
+        else{
+            std::cout << "Nie, to nie palindrom";
+        }
+    }
+
+    return 0;
+}
