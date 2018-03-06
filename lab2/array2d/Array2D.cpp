5903e1b5c9ed4fbdd44e4b2fecfaa9f3e2ca2f2c
ssysek <ssysek@gmail.com>
2018-03-06 09:34:19 +0100

    zajecia 2 v2

--- lab2/array2d/Array2D.cpp
+++ lab2/array2d/Array2D.cpp
@@ -23,6 +23,12 @@ int **Array2D(int n_rows, int n_columns){
 
 void DeleteArray2D(int **array, int n_rows, int n_columns) {
 
+//    for (int i=0; i < n_rows; ++i ){
+//        for(int j=0;j<n_columns;++j)
+//            delete array[i][j];}
+//    delete array;
+//    array = NULL;
+
 }
 
 void ShowTab(int n_rows, int n_columns, int *const *tab) {
