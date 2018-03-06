b694c069d9d8b6c90d8853b6ee130df2ed26056f
zasadjan <jan-zasadny@wp.pl>
2018-03-06 09:31:17 +0100

    Rozpoczęto simpleforwardlist i greatestproduct

---
+++ lab2/greatestproduct/main.cpp
@@ -0,0 +1,14 @@
+//
+// Created by zasadjan on 06.03.18.
+//
+#include <vector>
+#include <iostream>
+#include "GreatestProduct.h"
+int main(){
+    std::cout << GreatestProduct({1,2,3,0,6},2);
+
+
+
+    return 0;
+}
+
---
+++ lab2/simpleforwardlist/main.cpp
@@ -0,0 +1,6 @@
+//
+// Created by zasadjan on 06.03.18.
+//
+
+#include <iostream>
+#include "SimpleForwardList.h"
