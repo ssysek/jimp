d7aafded419825a423769f1cae34be9d4f4e5199
ssysek <ssysek@gmail.com>
2018-03-13 08:41:28 +0100

    CMake procjet for lab 3

---
+++ lab3/minimaltimedifference/MinimalTimeDifference.cpp
@@ -0,0 +1,9 @@
+//
+// Created by syskmaci on 13.03.18.
+//
+
+#include "MinimalTimeDifference.h"
+#include <vector>
+#include <sstream>
+#include <regex>
+#include <cmath>
---
+++ lab3/minimaltimedifference/MinimalTimeDifference.h
@@ -0,0 +1,17 @@
+//
+// Created by syskmaci on 13.03.18.
+//
+
+#ifndef JIMP_EXERCISES_MINIMALTIMEDIFFERENCE_H
+#define JIMP_EXERCISES_MINIMALTIMEDIFFERENCE_H
+#include <vector>
+#include <sstream>
+#include <regex>
+#include <cmath>
+
+unsigned int ToMinutes(std::string time_HH_MM);
+unsigned int MinimalTimeDifference(std::vector<std::string> times);
+
+
+
+#endif //JIMP_EXERCISES_MINIMALTIMEDIFFERENCE_H
