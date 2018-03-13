85476c2f58a466fa12adee74fd91fd67dcee27bf
ssysek <ssysek@gmail.com>
2018-03-13 09:08:45 +0100

    Min Time Diff 1.0

--- lab3/minimaltimedifference/MinimalTimeDifference.cpp
+++ lab3/minimaltimedifference/MinimalTimeDifference.cpp
@@ -7,7 +7,26 @@
 #include <sstream>
 #include <regex>
 #include <cmath>
+#include <iostream>
+#include <algorithm>
 
-unsigned int ToMinutes(std::string time_HH_MM){
+using namespace std;
 
+unsigned int MinimalTimeDifference(std::vector<std::string> times){
+    unsigned int min=1440;
+    vector<unsigned int> minutes;
+
+    for (auto time:times){
+        unsigned int tmp = ToMinutes(time);
+        minutes.emplace_back(tmp);
+    }
+
+    sort(minutes.begin(), minutes.end());
+
+    for( int k = 0 ; k<minutes.size() - 1; k++){
+        unsigned int diff = minutes[k] - minutes[k+1];
+        if (diff < min)
+            min = diff;
+    }
+    return min;
 }
