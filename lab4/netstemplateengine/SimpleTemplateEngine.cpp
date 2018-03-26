fc5cb0dbfed72258fe17cd9c4ba2269afd4be82a
Jan <jan-zasadny@wp.pl>
2018-03-26 17:42:17 +0200

    Dodano projekt netstemplateengine

---
+++ lab4/netstemplateengine/SimpleTemplateEngine.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Jan on 26.03.2018.
+//
+
+#include "SimpleTemplateEngine.h"
---
+++ lab4/netstemplateengine/SimpleTemplateEngine.h
@@ -0,0 +1,14 @@
+//
+// Created by Jan on 26.03.2018.
+//
+
+#ifndef JIMP_EXERCISES_SIMPLETEMPLATEENGINE_H
+#define JIMP_EXERCISES_SIMPLETEMPLATEENGINE_H
+
+
+class SimpleTemplateEngine {
+
+};
+
+
+#endif //JIMP_EXERCISES_SIMPLETEMPLATEENGINE_H
