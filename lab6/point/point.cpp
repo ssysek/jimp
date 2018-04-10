ed858f24c550881ad46ca3f1e88374c235823808
ssysek <ssysek@gmail.com>
2018-04-10 08:25:34 +0200

    Lab 6 start

---
+++ lab6/point/point.cpp
@@ -0,0 +1,44 @@
+//
+// Created by syskmaci on 10.04.18.
+//
+
+#include "point.h"
+#include <iomanip>
+#include <iostream>
+
+using ::std::istream;
+using ::std::ws;
+
+//Helper functions:
+void CheckNextChar(char c, istream* is) {
+    int next_char = is->peek();
+    if (next_char != c) {
+        throw runtime_error("invalid character");
+    }
+    is->ignore();
+}
+
+void IgnoreWhitespace(istream* is) {
+    (*is) >> ws;
+}
+
+double ReadNumber(istream* is) {
+    double d;
+    (*is) >> d;
+    return d;
+}
+
+
+// Właściwa definicja, obydwa argumenty funkcji nie
+//są zadeklarowane jako const, bo obydwa są modyfikowane
+//wewnątrz funkcji (STL nie używa naszej konwencji z przekazywaniem
+//przez wskaźnik)
+istream& operator>>(istream & input, Point& p){
+    CheckNextChar('(', &input);
+    p.SetX(ReadNumber(&input));
+    CheckNextChar(',', &input);
+    IgnoreWhitespace(&input);
+    p.SetY(ReadNumber(&input));
+    CheckNextChar(')', &input);
+    return input;      // Umożliwia cin >> a >> b >> c;
+}
