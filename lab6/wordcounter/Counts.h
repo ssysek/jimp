5bd2dba30d1064038518595c28f8e69a990f1e1a
Jan <jan-zasadny@wp.pl>
2018-04-16 18:18:25 +0200

    Stworzono klasę Counts

--- lab6/wordcounter/Counts.h
+++ lab6/wordcounter/Counts.h
@@ -5,10 +5,26 @@
 #ifndef JIMP_EXERCISES_COUNTS_H
 #define JIMP_EXERCISES_COUNTS_H
 
+namespace datastructures {
 
-class Counts {
+    class Counts {
 
-};
+    public:
 
+        friend class WordCounter;
+        Counts() {};
+        ~Counts() {};
+        Counts(int i);
+        Counts & operator++();
+        bool operator==(Counts zlicz) const ;
+        bool operator>(Counts zlicz) const ;
+        bool operator<(Counts zlicz) const ;
 
+    private:
+
+        int licznik;
+
+    };
+
+}
 #endif //JIMP_EXERCISES_COUNTS_H
