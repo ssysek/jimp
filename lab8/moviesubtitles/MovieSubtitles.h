c74ed560c17ca5c22bd37e7ac517b63b9effe0d2
Syska <ssysek@gmail.com>
2018-05-07 18:58:57 +0200

    Movie subtitles init

---
+++ lab8/moviesubtitles/MovieSubtitles.h
@@ -0,0 +1,19 @@
+//
+// Created by Sysek on 2018-05-07.
+//
+
+#ifndef JIMP_EXERCISES_MOVIESUBTITLES_H
+#define JIMP_EXERCISES_MOVIESUBTITLES_H
+
+
+
+#include <string>
+#include <iostream>
+#include <memory>
+#include <sstream>
+#include <regex>
+#include <stdexcept>
+
+
+
+#endif //JIMP_EXERCISES_MOVIESUBTITLES_H
