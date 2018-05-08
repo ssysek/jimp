ca5a205b1fcfd6acfc6f6c7e163a1236d210afe4
ssysek <ssysek@gmail.com>
2018-05-08 09:07:09 +0200

    serialization v2

--- lab9/academiaserializer/Serialization.h
+++ lab9/academiaserializer/Serialization.h
@@ -6,7 +6,23 @@
 #define JIMP_EXERCISES_SERIALIZATION_H
 
 
+namespace academia{
 
+    class Serializer;
+    class Serializable {
+    public:
+        Serializable() {};
+        virtual void Serialize(Serializer* serializer) const =0;
+    };
+
+    class Room:Serializable{
+        enum class Type {
+            COMPUTER_LAB,
+            LECTURE_HALL,
+            CLASSROOM
+        };
+    };
+}
 
 
 #endif //JIMP_EXERCISES_SERIALIZATION_H
