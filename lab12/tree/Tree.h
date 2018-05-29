f657e50cabcd0f325ef5d604f3c2177c8afeaf1d
ssysek <ssysek@gmail.com>
2018-05-29 09:12:23 +0200

    tree init

--- lab12/tree/Tree.h
+++ lab12/tree/Tree.h
@@ -6,9 +6,45 @@
 #define JIMP_EXERCISES_TREE_H
 
 
-class Tree {
+template <class T>
+bool Tree<T>::operator<(const Tree &other) const {
+    return this->value < other.value;
+}
 
-};
+
+
+template<class T>
+Tree<T>::Tree() {
+    depth= 0;
+    size = 0;
+    left = nullptr;
+    right = nullptr;
+}
+
+template<class T>
+void Tree<T>::Insert(T val) {
+    if (val < value) {
+        if (left != nullptr) left->Insert(val);
+        else {
+            std::unique_ptr<Tree<T>> new_node = std::make_unique<Tree<T>>();
+            new_node->value = val;
+            new_node->left = nullptr;
+            new_node->right = nullptr;
+            this->left = std::move(new_node);
+        }
+    }
+    else {
+        if ( right != nullptr) right->Insert(val);
+        else {
+            std::unique_ptr<Tree<T>> new_node = std::make_unique<Tree<T>>();
+            new_node->value = val;
+            new_node->left = nullptr;
+            new_node->right = nullptr;
+            this->right = std::move(new_node);
+        }
+    }
+    size++;
+}
 
 
 #endif //JIMP_EXERCISES_TREE_H
