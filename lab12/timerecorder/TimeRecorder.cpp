78731b9acd3e6dd12d2a39d4e548dd15874db447
Syska <ssysek@gmail.com>
2018-06-04 23:10:16 +0200

    cofka

--- lab12/timerecorder/TimeRecorder.cpp
+++ lab12/timerecorder/TimeRecorder.cpp
@@ -4,17 +4,17 @@
 
 #include "TimeRecorder.h"
 
-template <class T>
-auto TimeRecorder(T obj){
-    auto start=std::chrono::high_resolution_clock::now();
-
-    auto wart=obj();
-
-    auto stop=std::chrono::high_resolution_clock::now();
-
-    auto czas=stop-start;
-    return std::make_pair(wart,std::chrono::duration_cast<std::chrono::microseconds>(czas).count()*1000);
-
-
-
-}
+//
+//    template<class T>
+//    auto profiling::TimeRecorder(T obj) {
+//        auto start = std::chrono::high_resolution_clock::now();
+//
+//        auto wart = obj();
+//
+//        auto stop = std::chrono::high_resolution_clock::now();
+//
+//        auto czas = stop - start;
+//        return std::make_pair(wart, std::chrono::duration_cast<std::chrono::microseconds>(czas).count() * 1000);
+//
+//
+//    }
--- lab12/timerecorder/TimeRecorder.h
+++ lab12/timerecorder/TimeRecorder.h
@@ -12,11 +12,22 @@
 
 
 
-template <class T>
-auto TimeRecorder(T obj);
+#include <chrono>
+#include <iostream>
+#include <utility>
 
+namespace profiling {
 
+    template<class T>
+    auto TimeRecorder(T obj){
+        auto start = std::chrono::high_resolution_clock::now();
+        auto wart = (obj)();
+        auto stop = std::chrono::high_resolution_clock::now();
+        double czas = std::chrono::duration<double, std::milli>(stop-start).count();
+        return std::make_pair(wart, czas);
+    };
 
+}
 
 
 #endif //JIMP_EXERCISES_TIMERECORDER_H
