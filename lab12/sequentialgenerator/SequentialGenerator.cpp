75858190729e9ac8eb20a856ead1089f332809a2
Jan <jan-zasadny@wp.pl>
2018-06-04 20:41:45 +0200

    sequential generator

--- lab12/sequentialgenerator/SequentialGenerator.cpp
+++ lab12/sequentialgenerator/SequentialGenerator.cpp
@@ -3,3 +3,23 @@
 //
 
 #include "SequentialGenerator.h"
+
+SequentialIdGenerator::SequentialIdGenerator(){
+    head=true;
+    licznik=0;
+}
+
+SequentialIdGenerator::SequentialIdGenerator(TYP_PROSTY licznik_){
+    head=true;
+    licznik=licznik_;
+}
+
+TYP_ID SequentialIdGenerator::NextValue() {
+
+    if(!head){
+        licznik++;
+    }
+    head= false;
+    return TYP_ID (licznik);
+
+}
--- lab12/sequentialgenerator/SequentialGenerator.h
+++ lab12/sequentialgenerator/SequentialGenerator.h
@@ -8,7 +8,14 @@
 template <class TYP_ID, class TYP_PROSTY>
 class SequentialIdGenerator {
 public:
+    SequentialIdGenerator();
+    SequentialIdGenerator(TYP_PROSTY licznik_);
+    ~SequentialIdGenerator();
+    TYP_ID NextValue();
 
+private:
+    bool head;
+    TYP_PROSTY licznik;
 };
 
 
